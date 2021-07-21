d007921666b43ab3c5b560a02004f7c3d81fe549
Jake Stevens <jake.stevens@improving.com>
2021-07-21 10:30:18 -0500

    commit before scaffold

---
+++ DealerLead/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealerLead
+{
+	public class Vehicle
+	{
+		[Key]
+		[Column("VehicleId")]
+		public int Id { get; set; }
+
+		public int ModelId { get; set; }
+
+		public decimal MSRP { get; set; }
+
+		public string StockNumber { get; set; }
+
+		public string Color { get; set; }
+
+		public string DealershipId { get; set; }
+
+		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+		public DateTime CreateDate { get; set; }
+
+		public DateTime? ModifyDate { get; set; }
+
+		public DateTime? SellDate { get; set; }
+
+		public SupportedModel SupportedModel {  get; set; }
+	}
+}
